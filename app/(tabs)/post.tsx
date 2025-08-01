f075a65ee8bf960048871c23489ec507a9d18ef9
kimbohyun <bhyn@kimbohyuns-MacBook-Air.local>
2025-08-01 22:09:34 +0900

    FEAT: 포스트 작성 UI

--- app/(tabs)/post.tsx
+++ app/(tabs)/post.tsx
@@ -139,6 +139,29 @@ export default function postPage() {
           )}
         </ScrollView>
       </View>
+      <Pressable //임시로 추가해 둠!!
+        style={{
+          position: 'absolute',
+          bottom: 30,
+          right: 30,
+          width: 60,
+          height: 60,
+          borderRadius: 30,
+          backgroundColor: '#FF5C5C',
+          justifyContent: 'center',
+          alignItems: 'center',
+          elevation: 5,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.3,
+          shadowRadius: 4,
+          zIndex: 100,
+        }}
+        onPress={() => router.push('/postWrite')}
+      >
+        <Filter />
+      </Pressable>
+
       <PostFilterModal
         visible={isFilterVisible}
         onClose={() => setFilterVisible(false)}
