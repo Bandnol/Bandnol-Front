93f770dcfe36859beb2dd2ceb4e447a54eacf94c
kimbohyun <bhyn@kimbohyuns-MacBook-Air.local>
2025-08-04 22:51:23 +0900

    MOD: 알림센터 무한스크롤 구현

--- app/(tabs)/music-recommend/alarmCenter.tsx
+++ app/(tabs)/music-recommend/alarmCenter.tsx
@@ -1,12 +1,14 @@
 import Backarrow from '@/assets/icons/size_m/backarrow.svg';
 import { IconType } from '@/components/NotificationIcon';
+import type { NotificationItemProps } from '@/components/NotificationItem';
 import NotificationItem from '@/components/NotificationItem';
 import { Typography } from '@/constants/typography';
 import axios from 'axios';
 import { useRouter } from 'expo-router';
 import * as SecureStore from 'expo-secure-store';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import {
+  ActivityIndicator,
   Dimensions,
   FlatList,
   Pressable,
@@ -14,62 +16,71 @@ import {
   Text,
   View,
 } from 'react-native';
-type Notification = {
-  id: string;
-  createdAt: string;
-  type: string;
-  isConfirmed: boolean;
-  link: string;
-  sender: {
-    id: string;
-    nickname: string;
-  } | null;
-  content: string | null;
-};
+
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function AlarmCenterPage() {
   const router = useRouter();
-  const [notifications, setNotifications] = useState<Notification[]>([]);
-  useEffect(() => {
-    const fetchNotifications = async () => {
-      try {
-        const token = await SecureStore.getItemAsync('JWTToken');
-        if (!token) throw new Error('JWT 토큰 없음');
 
-        const response = await axios.get(
-          'https://bandnol.app/api/v1/users/me/notification',
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          },
-        );
-        if (response.data.success) {
-          setNotifications(response.data.data.data); //?
-        } else {
-          console.error('서버 응답 오류:', response.data.error);
-          setNotifications([]);
-        }
-      } catch (error) {
-        console.error('알림 가져오기 실패:', error);
-        setNotifications([]);
+  const [notifications, setNotifications] = useState<NotificationItemProps[]>(
+    [],
+  );
+  const [cursor, setCursor] = useState<string | null>(null);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const fetchNotifications = useCallback(async () => {
+    if (isFetching || !hasNextPage) return;
+    setIsFetching(true);
+
+    try {
+      const token = await SecureStore.getItemAsync('JWTToken');
+      if (!token) throw new Error('JWT 토큰 없음');
+
+      const apiUrl = `https://bandnol.app/api/v1/users/me/notification`;
+      const queryParams = cursor ? { cursor } : {};
+
+      const response = await axios.get(apiUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: queryParams,
+      });
+
+      if (response.data.success) {
+        const newNoti = response.data.data.data;
+        const NextCursor = response.data.data.nextCursor;
+
+        setNotifications((prev) => [...prev, ...newNoti]);
+        setCursor(NextCursor);
+        setHasNextPage(response.data.data.hasNext);
+      } else {
+        console.error('서버 오류:', response.data.error);
       }
-    };
+    } catch (error: any) {
+      console.error(
+        '알림 가져오기 실패:',
+        error?.response || error?.message || error,
+      );
+    } finally {
+      setIsFetching(false);
+    }
+  }, [cursor, isFetching, hasNextPage]);
 
+  useEffect(() => {
     fetchNotifications();
   }, []);
+
+  const handleEndReached = () => {
+    if (!isFetching && hasNextPage) {
+      fetchNotifications();
+    }
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Pressable
-          onPress={() => {
-            router.back();
-            console.log('Back pressed');
-          }}
-          style={styles.backArrow}
-        >
+        <Pressable onPress={() => router.back()} style={styles.backArrow}>
           <Backarrow width={22} height={18} />
         </Pressable>
         <Text style={styles.title}>알림</Text>
@@ -82,7 +93,7 @@ export default function AlarmCenterPage() {
           <NotificationItem
             id={item.id}
             createdAt={item.createdAt}
-            type={item.type as IconType} // 타입 명시적 변환 필요
+            type={item.type as IconType}
             isConfirmed={item.isConfirmed}
             link={item.link}
             sender={item.sender}
@@ -90,6 +101,11 @@ export default function AlarmCenterPage() {
           />
         )}
         contentContainerStyle={styles.list}
+        onEndReached={handleEndReached}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={
+          isFetching ? <ActivityIndicator size="small" color="#fff" /> : null
+        }
       />
     </View>
   );
@@ -101,7 +117,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     paddingTop: 60,
     paddingHorizontal: 20,
-    //paddingBottom: 10,
   },
   header: {
     height: 64,
@@ -110,7 +125,6 @@ const styles = StyleSheet.create({
     position: 'relative',
     backgroundColor: '#000',
   },
-
   title: {
     ...Typography.subtitle1B,
     position: 'absolute',
@@ -119,7 +133,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#fff',
   },
-
   backArrow: {
     position: 'absolute',
     left: 0,
@@ -127,7 +140,6 @@ const styles = StyleSheet.create({
     transform: [{ translateY: -9 }],
     zIndex: 1,
   },
-
   list: {
     paddingTop: 10,
     paddingBottom: 10,
