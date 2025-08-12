179caf46f452e32fa1007f64182e4ac449a89a0d
simchaemin <chaimin1001@naver.com>
2025-08-12 20:07:16 +0900

    MOD: 중간 저장

--- app/(tabs)/music-recommend/receiveRecommend.tsx
+++ app/(tabs)/music-recommend/receiveRecommend.tsx
@@ -1,4 +1,4 @@
-import { API_URL, TEST_TOKEN } from '@env';
+import { API_URL, EXPO_PUBLIC_API_TOKEN } from '@env';
 import axios from 'axios';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useEffect, useState } from 'react';
@@ -24,7 +24,7 @@ const api = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${TEST_TOKEN}`, // .env에 있는 토큰
+    Authorization: `Bearer ${EXPO_PUBLIC_API_TOKEN}`, // .env에 있는 토큰
   },
 });
 
