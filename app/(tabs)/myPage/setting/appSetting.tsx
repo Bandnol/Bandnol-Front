95486479ee491b03cbef4203b85b6f431ab18672
DADAIZGOOD <dachord@hanyang.ac.kr>
2025-08-15 02:46:04 +0900

    FEAT: 설정/알림 API 연동

--- app/(tabs)/myPage/setting/appSetting.tsx
+++ app/(tabs)/myPage/setting/appSetting.tsx
@@ -119,7 +119,7 @@ export default function AppSetting() {
           </TouchableOpacity>
         </View>
         {/* 최근 알림 미리보기 */}
-        <View style={{ paddingHorizontal: 20, paddingTop: 12 }}>
+        {/* <View style={{ paddingHorizontal: 20, paddingTop: 12 }}>
           <Text style={styles.sectionTitle}>최근 알림</Text>
           {loading ? (
             <ActivityIndicator style={{ marginTop: 16 }} />
@@ -151,8 +151,8 @@ export default function AppSetting() {
                 </View>
               )}
             />
-          )}
-        </View>
+          )} // 왜 있는 거지???
+        </View> */}
       </View>
     </SafeAreaView>
   );
