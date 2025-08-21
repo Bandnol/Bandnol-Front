9610421d0848932868024ab61be1028862e7acd5
Youngju Lee <oortmealy@gmail.com>
2025-08-22 00:39:20 +0900

    아티스트 채널 ui 수정 및 api 변동 반영

--- app/(tabs)/recommend-tab/RecShareModal.tsx
+++ app/(tabs)/recommend-tab/RecShareModal.tsx
@@ -19,7 +19,7 @@ import {
 } from 'react-native';
 import Modal from 'react-native-modal';
 
-import Share, { Social } from 'react-native-share';
+//import Share, { Social } from 'react-native-share';
 
 import Svg, { Circle } from 'react-native-svg';
 import ViewShot from 'react-native-view-shot';
@@ -54,7 +54,7 @@ export default function RecShareModal({
     await Clipboard.setStringAsync(shareUrl);
     Alert.alert('링크가 복사되었습니다.');
   };
-    const handleShareToInstagramStory = async () => {
+  const handleShareToInstagramStory = async () => {
     if (Platform.OS === 'ios') {
       try {
         if (!recData) return;
