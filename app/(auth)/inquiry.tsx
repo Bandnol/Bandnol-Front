33c0045f75e23088eaf8c01188358e4d9266163d
kimbohyun <bhyn@kimbohyuns-MacBook-Air.local>
2025-08-22 01:37:02 +0900

    FIX: 문의하기 컨펌 모달

--- app/(auth)/inquiry.tsx
+++ app/(auth)/inquiry.tsx
@@ -16,6 +16,7 @@ import BackIcon from '@/assets/auth/inquiry/Vector.svg';
 import { Colors } from '@/constants/Colors';
 import { Typography } from '@/constants/typography';
 import api from '@/store/api'; // axios 인스턴스
+import CheckCircle from '@/assets/icons/checkCircle.svg';
 
 export const options = {
   headerShown: false,
@@ -27,6 +28,7 @@ const Component = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [content, setContent] = useState('');
+  const [successVisible, setSuccessVisible] = useState(false);
 
   const goBackSmart = () => {
     if (returnTo && typeof returnTo === 'string') {
@@ -59,8 +61,11 @@ const Component = () => {
         email,
         content,
       });
-      alert('문의가 성공적으로 전송되었습니다.');
-      goBackSmart();
+      setSuccessVisible(true);
+      setTimeout(() => {
+        setSuccessVisible(false);
+        goBackSmart();
+      }, 2000);
     } catch (error: any) {
       console.error('문의 전송 실패:', error);
       alert('문의 전송에 실패했어요. 다시 시도해주세요.');
@@ -157,6 +162,21 @@ const Component = () => {
             <InquiryButton width={335} height={50} />
           </TouchableOpacity>
         </View>
+
+        {successVisible && (
+          <View style={styles.toastOverlay} pointerEvents="none">
+            <View style={styles.toastBox}>
+              <View style={{ width: 42, height: 42 }}>
+                <CheckCircle width={31.5} height={31.5} />
+              </View>
+
+              <Text style={styles.toastText}>
+                문의 내용이 정상적으로 접수되었습니다.{'\n'}
+                빠른 시일 내에 답변 드리겠습니다.
+              </Text>
+            </View>
+          </View>
+        )}
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
@@ -293,6 +313,33 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 812,
     overflow: 'hidden',
+  },
+  toastOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.5)', // 반투명 배경
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 999,
+  },
+  toastBox: {
+    width: '82%',
+    backgroundColor: '#3A3A3A',
+    borderRadius: 10,
+    paddingVertical: 40,
+    paddingHorizontal: 16,
+    alignItems: 'center',
+    gap: 10,
+  },
+
+  toastText: {
+    color: '#F4F4F4',
+    textAlign: 'center',
+    fontFamily: 'Pretendard',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    letterSpacing: -0.48, // px 아님: 숫자
+    flexShrink: 1,
   },
 });
 
