a688fbdc46969fdfae7e81b917bd22ab1abed7ed
Youngju Lee <oortmealy@gmail.com>
2025-08-23 02:43:37 +0900

    Uncomment import for react-native-share
    주석처리 제거

--- app/(tabs)/recommend-tab/RecShareModal.tsx
+++ app/(tabs)/recommend-tab/RecShareModal.tsx
@@ -22,7 +22,7 @@ import {
 } from 'react-native';
 import Modal from 'react-native-modal';
 
-//import Share, { Social } from 'react-native-share';
+import Share, { Social } from 'react-native-share';
 
 import Svg, { Circle } from 'react-native-svg';
 import ViewShot from 'react-native-view-shot';
